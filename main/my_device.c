350d9d3c941e9044aa2313f56218dd266c8effa8
Volodymyr Kozariz <dovakin746@gmail.com>
2026-02-10 05:13:19 +0000

    feature: added led blink loop

--- main/my_device.c
+++ main/my_device.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#define BLINK_GPIO 2 
 
 void app_main(void)
 {
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
+    ESP_LOGI("APP", "Starting the blink loop...");
+
+    while (1) {
+        ESP_LOGI("APP", "Turning LED ON");
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(pdMS_TO_TICKS(100)); // Wait 100 ms
+
+        ESP_LOGI("APP", "Turning LED OFF");
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(pdMS_TO_TICKS(100)); // Wait 100 ms
+    }
 }
